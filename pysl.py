5be3836c6aca0bb27cd9ca4119d4a146fd528663
Brian Fee <brianfee@protonmail.com>
2020-08-19 10:55:28 -0600

    Modified create_fifo to take fifo location as argument

--- pysl.py
+++ pysl.py
@@ -4,16 +4,20 @@
 
 import os
 
-def create_fifo():
+def create_fifo(fifo):
+    """ Create a first in, first out pipe. """
     pid = os.getpid()
     fifo = f'/tmp/pysl_input.{pid}'
     os.mkfifo(fifo)
-    return fifo
 
 def main():
     """ Main function for pysl """
     print('pysl launched...')
-    print(f'Pipe created at {create_fifo()}')
+
+    fifo = f'/tmp/pysl_input.{os.getpid()}'
+
+    create_fifo(fifo)
+    print(f'Pipe created at {fifo}')
 
 if __name__ == '__main__':
     main()
