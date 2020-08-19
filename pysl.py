d065fd31eb70ba4b29c92e5d4597c9bd4c966b94
Brian Fee <brianfee@protonmail.com>
2020-08-19 14:17:22 -0600

    Added argument parser, option to write to pipe

--- pysl.py
+++ pysl.py
@@ -2,13 +2,15 @@
 
 """ Main status line script. """
 
+import argparse
 import os
 import signal
 import sys
 
 FIFO = f'/tmp/pysl.{os.getpid()}'
 
-def signal_handler(sig, frame):
+def cleanup(sig, frame): # pylint: disable=unused-argument
+    """ Cleanup on OS signals. """
     delete_fifo(FIFO)
     sys.exit(0)
 
@@ -23,23 +25,56 @@ def read_fifo(fifo):
             data = pipe.read()
 
             if len(data) == 0:
-                break;
+                break
 
             print(data, end='')
+
+def write_fifo(fifo, output):
+    """ Open and append output to a FIFO pipe. """
+    print(fifo)
+    with open(fifo, 'w') as pipe:
+        pipe.write(output + '\n')
+        pipe.flush()
 
 def delete_fifo(fifo):
     """ Delete a FIFO pipe. """
     os.remove(fifo)
 
+def parse_arguments():
+    """ Parses command line arguments. """
+    desc = """Python Status Line"""
+    parser = argparse.ArgumentParser(description=desc)
+
+    # Positional Arguments
+    parser.add_argument('text', type=str, default='', nargs='?')
+
+    # Optional Arguments
+    parser.add_argument('-p', '--pid', help='specify process id', type=str,
+                        metavar='PID')
+    parser.add_argument('-w', '--watch', help='run program in watcher mode.',
+                        action='store_true')
+
+    return parser.parse_args()
+
+
 def main():
     """ Main function for pysl """
+
+    args = parse_arguments()
+
+    print(args.pid)
+    print(args.text)
+    if not args.watch:
+        write_fifo(f'/tmp/pysl.{args.pid}', args.text)
+        sys.exit()
+
     print('pysl launched...')
     sys.stdout.flush()
     create_fifo(FIFO)
 
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.signal(signal.SIGHUP, signal_handler)
-    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGINT, cleanup)
+    signal.signal(signal.SIGHUP, cleanup)
+    signal.signal(signal.SIGTERM, cleanup)
 
     while True:
         read_fifo(FIFO)
